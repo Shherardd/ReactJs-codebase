bd7515434d2591db40622bf8a3e5d00cc85a0052
Gerardo López <gerardwjones@gmail.com>
2020-08-01 23:36:54 -0500

    page styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,13 @@ import './styles/Navbar.css'
 class Navbar extends React.Component {
     render() {
         return <div className="Navbar">
-            <a href = '/'>
-                <img src={logo} alt='logo'/>
-                <span className='font-weight-light'>Bagdes </span>
-                <span className='font-weight-bold'>Gen</span>
-            </a>
+            <div className="container-fluid">
+                <a className="Navbar__brand" href = '/'>
+                    <img src={logo} className="Navbar__logo Navbar_brand-logo" alt='logo'/>
+                    <span className='font-weight-light'>Bagdes </span>
+                    <span className='font-weight-bold'>Gen</span>
+                </a>
+            </div>
         </div>
     }
 }
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Badge from '../components/badge'
 import Navbar from '../components/Navbar'
+import './styles/BadgeNew.css'
+import header from '../images/logo.png'
 
 class BadgeNew extends React.Component {
     render() {
         return (
             <div>
                 <Navbar/>
+                <div className="BadgeNew__hero">
+                    <img src={header} className="img-fluid" alt="logo"/>
+                </div>
             </div>
             
         )
