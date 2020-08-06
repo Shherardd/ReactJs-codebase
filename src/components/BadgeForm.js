9740bcb1c70312054c9d52e31ea23d22b9df8fd1
Gerardo López <gerardwjones@gmail.com>
2020-08-05 23:55:24 -0500

    input forms in BadgeForms

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -20,6 +20,22 @@ class BadgeForm extends React.Component {
                         <label htmlFor="">First Name</label>
                         <input onChange={this.handleChange} className="form-control" id="firstNameInput" type="text" name="firstName"></input>
                     </div>
+                    <div className='form-group'>
+                        <label htmlFor="">Last Name</label>
+                        <input onChange={this.handleChange} className="form-control" id="lastNameInput" type="text" name="lastName"></input>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="">Email</label>
+                        <input onChange={this.handleChange} className="form-control" id="emailInput" type="text" name="email"></input>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="">Job Title</label>
+                        <input onChange={this.handleChange} className="form-control" id="jobTitleInput" type="text" name="jobTitle"></input>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="">Twitter</label>
+                        <input onChange={this.handleChange} className="form-control" id="twitterInput" type="text" name="twitter"></input>
+                    </div>
                     <div>
                         <button type="button" onClick={this.handleClick} className="btn btn-primary">Save</button>
                     </div>
